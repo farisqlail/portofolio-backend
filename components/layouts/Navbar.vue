f12981be2edf9e43b174df38f0a5ca7a4b420e0d
farisqlail <faris.riskilail@gmail.com>
2021-08-07 12:57:40 +0700

    blog

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -56,21 +56,20 @@
 
 <script>
 export default {
-//   data() {
-//     return {
-//       items: [
-//         {
-//           title: "Home",
-//           icon: "home",
-//           to: { name: "index" }
-//         },
-//         {
-//           title: "Inspire",
-//           icon: "lightbulb",
-//           to: { name: "inspire" }
-//         }
-//       ]
-//     };
-//   }
+    data() {
+      return {
+        items: [
+          {
+            title: "Beranda",
+            to: { name: "index" }
+          },
+          {
+            title: "Inspire",
+            icon: "lightbulb",
+            to: { name: "inspire" }
+          }
+        ]
+      };
+    }
 };
 </script>
