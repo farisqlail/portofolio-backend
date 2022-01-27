7c944f67e4964124875d02e6b3cbdf4d4e671d05
farisqlail <faris.riskilail@gmail.com>
2022-01-27 11:16:34 +0700

    halo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,7 +64,7 @@
 
     <div class="container">
         <section id="home">
-            <h2>Hallo, Saya Apriliani</h2>
+            <h2>Hallo, Saya Faris Rizqilail</h2>
             <h5>
                 Salam kenal, saya web developer, designer, illustrator, dan sedang
                 belajar menulis blog di Medium
