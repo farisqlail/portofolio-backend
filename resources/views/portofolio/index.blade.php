8060a90f58305a484e7912aa3ea5f4d5d134d233
farisqlail <faris.riskilail@gmail.com>
2022-01-28 23:13:24 +0700

    data in index portfolio

--- resources/views/portofolio/index.blade.php
+++ resources/views/portofolio/index.blade.php
@@ -2,22 +2,24 @@
 
 @section('content')
 
-<div class="container">
-    <section id="blog">
-      {{-- <h2>
+    <div class="container">
+        <section id="blog">
+            {{-- <h2>
         Blog
       </h2>
       <br /><br /> --}}
+            @foreach ($portfolio as $item)
 
-      <h3>
-        Membuat Anak
-      </h3>
-      <small class="text-muted"><i>Website</i></small>
-      <p class="mt-3">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur magni suscipit nulla optio velit! Neque eligendi dignissimos cupiditate aliquam facere. Praesentium non porro similique inventore officia harum dolorem minima voluptatem.
-      </p>
-      <a href="">Lihat Selengkapnya</a>
-    </section>
-  </div>
+                <h3>
+                    {{ $item->title }}
+                </h3>
+                <small class="text-muted"><i>{{ $item->kategori }}</i></small>
+                <p class="mt-3">
+                    {{ $item->description }}
+                </p>
+                <a href="{{ $item->link }}">Lihat Selengkapnya</a>
+            @endforeach
+        </section>
+    </div>
 
 @endsection
